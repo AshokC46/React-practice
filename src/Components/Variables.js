bc6323ac91634943f9576c61a03fd1614eb421b9
Ashok <ashokchavala46@gmail.com>
2025-07-26 15:01:35 +0530

    first copy

---
+++ src/Components/Variables.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Variables() {
+    let name = "hello world hello";
+  return (
+    <div>
+      <h1>Hello world {name}</h1>
+    </div>
+  )
+}
+export default Variables;
